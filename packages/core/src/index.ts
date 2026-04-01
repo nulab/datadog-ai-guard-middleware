71c068c27fddc44437f4e9f8adc2a84eb0f53287
vvatanabe <yuichi.watanabe.ja@gmail.com>
2026-04-01 12:16:49 +0900

    Refactor exports in index.ts to streamline AIGuard evaluation engine and remove unused types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,22 +6,12 @@ export {
 } from "./convert.js";
 
 export { AIGuardAbortError, AIGuardClientError, AIGuardStreamTripWire } from "./errors.js";
-export type { AIGuardEvaluationEngineOptions } from "./evaluator.js";
-export {
-  AIGuardEvaluationEngine,
-  getErrorMessage,
-  hasMeaningfulAssistantMessages,
-  normalizeAIGuardAction,
-} from "./evaluator.js";
+export { AIGuardEvaluationEngine } from "./evaluator.js";
 export type {
-  AIGuardContentPart,
   AIGuardEvaluator,
-  AIGuardImageUrlContentPart,
   AIGuardKind,
   AIGuardLogger,
   AIGuardMessage,
   AIGuardMiddlewareOptions,
-  AIGuardTextContentPart,
-  CoreContentPart,
   ToolCallPartInput,
 } from "./types.js";
